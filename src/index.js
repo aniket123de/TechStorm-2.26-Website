b20f7d0ed748393b2bdef8eabbf6c02ae8b1e1d5
aniket123de <aniketde1234@gmail.com>
2026-02-02 10:37:26 +0530

    Font Fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import 'pixel-retroui/dist/fonts.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/animate.min.css';
 import './assets/css/dripicons.css';
